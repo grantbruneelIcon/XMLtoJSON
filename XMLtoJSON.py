bd155e450fbe98d73e743f1676e86e7a2cdb072b
Grant Bruneel <Grant.Bruneel@iconfitness.com>
2020-04-28 14:59:46 -0600

    Single testsuite put into JSON format

--- XMLtoJSON.py
+++ XMLtoJSON.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import xml.etree.ElementTree as ET
+import json
 
 # From the user, ask for File Location until an Existing file is given
 def getXMLfilesInput():
@@ -8,13 +9,12 @@ def getXMLfilesInput():
         user_input = input("File Location: ")
         location = Path(user_input)
         if location.exists():
-            print("File Found")
             if (getXMLfiles(location)):
                 return "Done"
             else:
                 print("No XML file found at this location, try again or type \"Quit\" to exit")
         if user_input.lower() == "quit":
-            return "Script Ended"
+            return "Script quit"
         elif not location.exists():
             print("No File Exists, please try again")
 
@@ -29,16 +29,24 @@ def getXMLfiles(location):
     else:
         location = str(location.resolve())
         if  not location.endswith("xml"):
-            print(location + " is not XML")
             return False
-        else:
-            print(location + " is XML")
-            print(ET.parse(location))
+        else: # XML file is found here it is converted to a JSON file
+            return convert(location)
+            
 
-            return True
-    
+def convert(file):
+    tree = ET.parse(file)
+    testsuits = tree.getroot()
+    testsuit = testsuits.getchildren()[0]
+    jsonfile = open("sample.json", "w")
+    jsonfile.write("{")
+    jsonfile.writelines(json.dumps(testsuit.tag) + ":")
+    jsonfile.writelines(json.dumps(testsuit.attrib))
+    jsonfile.write("}")
+
+    jsonfile.close()
 
 def main():
     print(getXMLfilesInput())
 
-main()
+print(convert("PASSED_ETPF44920_83_309_2020_03_24_16_10.xml"))
